e449ad0c05d868715ff3d93ce26e75a188035717
userme55 <70228664+userme55@users.noreply.github.com>
2020-08-29 22:56:04 +0500

    Update client.js

--- app/static/client.js
+++ app/static/client.js
@@ -29,7 +29,7 @@ function analyze() {
   xhr.onload = function(e) {
     if (this.readyState === 4) {
       var response = JSON.parse(e.target.responseText);
-      el("result-label").innerHTML.style.color = 'white' = `Result = ${response["result"]};
+      el("result-label").innerHTML = `Result = ${response["result"]}.html('<span style="color:white">test</span>');
     }
     el("analyze-button").innerHTML = "Analyze";
   };
